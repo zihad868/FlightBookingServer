3892c13f227b03f49581bcc5785666cf0d57fcdd
zihad868 <zihadh868@gmail.com>
2024-11-19 09:46:22 +0600

    MongoDB database connection

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require('express');
+const app = express();
+require('dotenv').config();
 
-const port = 5000 | process.env.PORT
+const port = process.env.PORT || 5000;
+
+// Connect MongoDB
+const connectMongoDB = require('./Config/db');
+connectMongoDB();
 
 
 app.get('/', (req, res) =>{
-    res.send("Get Request")
+    res.send("Get Request");
 })
 
 app.listen(port, () => {
-    console.log(`Flight booking server is running on port ${port}`)
+    console.log(`Flight booking server is running on port ${port}`);
 })
